06e756b04c897d5eb051d3d309bd6198271060bb
Abdulbaki Adabara Suraj <surajabdulbaki19@gmail.com>
2020-12-22 13:53:22 +0100

    Update banner.php
    Allow banner.php to check if user is logged in and display html based on role

--- includes/banner.php
+++ includes/banner.php
@@ -1,5 +1,5 @@
 <div class="banner">
-
+        <?php if(!isset($_SESSION['user']): ?>
 	<!-- Banner display for Visitor -->
 	<div class="unregistered">
 		<div class="quotes">
@@ -18,5 +18,17 @@
 		</div>
 	</div>
 	<!-- // Banner display for Visitor -->
-	
+        
+        <?php else if(isset($_SESSION['user']) && $_SESSION['user']['role'] == 'author'): ?>
+        <!-- Banner display for Basic -->
+	<div class="registered">
+		<div class="welcome">
+		        Welcome {username}
+		</div>
+		<div class="panel link">
+			<a href="<?php echo BASE_URL.'/signup.php/?ref='.current_url()&type=2; ?>">Become An Author</a>
+		</div>
+	</div>
+	<!-- // Banner display for Basic -->
+	<?php endif; ?>
 </div>
