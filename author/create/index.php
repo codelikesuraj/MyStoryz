27e67760afc3aded85f9370eae57b9b24a48b4c7
Abdulbaki <surajabdulbaki19@gmail.com>
2021-01-10 19:54:54 +0100

    Added files
    Added create story feature for author
    Added manage story feature for author

--- author/create/index.php
+++ author/create/index.php
@@ -15,6 +15,33 @@
 		<?php include(ROOT_PATH.'/author/author_sidebar.php'); ?>
 		<!-- // Display sidebar -->
 
+		<!-- Display story creation form -->
+		<h2>Create Story</h2>
+		<form method="post" autocomplete="off">
+			<label>
+				Title<br/>
+				<input type="text" name="story_title" placeholder="Add story title ..." />
+			</label><br/><br/>
+			<label>
+				Add story image (jpg/png)<br/>
+				<input type="file" name="story_image" />
+			</label><br/><br/>
+			<label>
+				Body<br/>
+				<textarea name="story_content" placeholder="Story goes here ..." cols="40" rows="20"></textarea>
+			</label><br/><br/>
+			<label>
+				Set status: 
+				<select name="story_status">
+					<option value="0" selected>Unpublish</option>
+					<option value="1">Publish</option>
+				</select>
+			</label><br/><br/>
+			<a href="<?php echo BASE_URL.'/author'; ?>">Cancel</a>
+			<input type="submit" name="create_story" value="Create" />
+		</form>
+		<!-- Display story creation form -->
+
 		<!-- Display footer -->
 		<?php include(ROOT_PATH.'/includes/footer.php'); ?>
 		<!-- // Display footer -->
--- author/manage/index.php
+++ author/manage/index.php
@@ -15,14 +15,103 @@
 		<?php include(ROOT_PATH.'/author/author_sidebar.php'); ?>
 		<!-- // Display sidebar -->
 
-		<!-- Display story creation form -->
-		<form method="post" autocomplete="off">
-			<label>
-				Title<br/>
-				<input type="text" name="title" />
-			</label><br/>
-		</form>
-		<!-- Display story creation form -->
+		<!-- Display management table -->
+		<div class="management-table" style="height: 50vw;overflow: scroll;">
+			<table border="1">
+				<thead>
+					<th>S/N</th>
+					<th>TITLE</th>
+					<th>COMMENTS</th>
+					<th>STATUS</th>
+					<th>PUBLISHED</th>
+					<th>MODIFIED</th>
+					<th colspan="2">ACTION</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Wonders of medical science</td>
+						<td>52</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Africa and the internet</td>
+						<td>0</td>
+						<td>Not published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Unfinished house</td>
+						<td>876</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>4</td>
+						<td>Nigeria and the Olympics</td>
+						<td>14754</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>5</td>
+						<td>Wonders of medical science</td>
+						<td>52</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>6</td>
+						<td>Africa and the internet</td>
+						<td>0</td>
+						<td>Not published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>7</td>
+						<td>Unfinished house</td>
+						<td>876</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+					<tr>
+						<td>8</td>
+						<td>Nigeria and the Olympics</td>
+						<td>14754</td>
+						<td>Published</td>
+						<td>00:00:00 00/00/00</td>
+						<td>00:00:00 00/00/00</td>
+						<td>edit</td>
+						<td>delete</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- // Display management table -->
 
 		<!-- Display footer -->
 		<?php include(ROOT_PATH.'/includes/footer.php'); ?>
